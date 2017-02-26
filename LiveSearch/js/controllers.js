44c4b54c0b183c04780d7ad60e0f0061a1720d6c
Pavel <parfum505@gmail.com>
2017-02-26 17:54:15 +0100

    add search block

--- LiveSearch/js/controllers.js
+++ LiveSearch/js/controllers.js
@@ -2,11 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http) {
 
-	$scope.users;
 	$http.get('js/data.json').then(function(data){
 		$scope.users = data;
-			console.log($scope.users.data[0]);
 	});
+	$scope.userOrder = 'name';
 
 
 
