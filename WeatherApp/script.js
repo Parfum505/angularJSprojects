e159f6d15672f2ac9e79ed647c5b672f4056105b
Pavel <parfum505@gmail.com>
2017-02-28 16:44:59 +0100

    start new project Weather App

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,41 +1,21 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
 
-myApp.config(function($routeProvider){
+myApp.config(function ($routeProvider) {
 	$routeProvider
-	.when('/', {
-		templateUrl: 'pages/main.html',
-		controller: 'mainController'
-	})
-	.when('/second', {
-		templateUrl: 'pages/second.html',
-		controller: 'secondController'
-	})
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'homeController'
+		})
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		})
 });
 
-myApp.controller('mainController', ['$scope', '$timeout','$filter', '$routeParams', function ($scope, $timeout, $filter, $routeParams) {
-	$scope.name = "Hello!";
-
-	$timeout(function () {
-		$scope.name = "Goodbye!";
-	}, 2000);
-	$scope.text = '';
-	$scope.lowerText = function () {
-		return $filter('lowercase')($scope.text);
-	}
-	$scope.limit = 5;
-	$scope.rules = ['be nice', 'be cool', 'be brave'];
-	$scope.alertClick = function () {
-		alert('I click ' + this.rule);
-		console.log(this.$parent);
-	}
-}]);
-myApp.controller('firstController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-	// $scope.name = "Hello!";
-
+myApp.controller('homeController', ['$scope', function($scope) {
 
 }]);
-myApp.controller('secondController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-	// $scope.name = "Hello!";
 
+myApp.controller('forecastController', ['$scope', function($scope) {
 
 }]);
