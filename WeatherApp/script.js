8370fec21aea262c86e3b5c242c8e7cd45769d1a
Pavel <parfum505@gmail.com>
2017-02-28 22:03:08 +0100

    add custom service and http

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -12,10 +12,22 @@ myApp.config(function ($routeProvider) {
 		})
 });
 
-myApp.controller('homeController', ['$scope', function($scope) {
+myApp.service('cityService', function() {
+	this.city = '';
+});
 
+myApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	});
 }]);
 
-myApp.controller('forecastController', ['$scope', function($scope) {
+myApp.controller('forecastController', ['$scope','$http', 'cityService', function($scope, $http, cityService) {
 
+	$scope.city = cityService.city;
+	$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+$scope.city+'&cnt=7&APPID=0867a13b59c398d1edd05d49f440e4f0&').then(function(respons) {
+		//$scope.temp = respons.list[0].temp.day;
+		console.log(respons);
+	});
 }]);
