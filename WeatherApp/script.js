4b2d50a98d74c72ea95d16f26a2c59a85fd4defc
Pavel <parfum505@gmail.com>
2017-03-01 21:32:41 +0100

    add css and html

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
 
 myApp.config(function ($routeProvider) {
 	$routeProvider
-		.when('/', {
+		.when('/home', {
 			templateUrl: 'pages/home.html',
 			controller: 'homeController'
 		})
@@ -10,6 +10,9 @@ myApp.config(function ($routeProvider) {
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
 		})
+		.otherwise({
+			redirectTo: '/home'
+		});
 });
 
 myApp.service('cityService', function() {
@@ -24,10 +27,18 @@ myApp.controller('homeController', ['$scope', 'cityService', function($scope, ci
 }]);
 
 myApp.controller('forecastController', ['$scope','$http', 'cityService', function($scope, $http, cityService) {
-
+	$scope.limit = 3;
 	$scope.city = cityService.city;
-	$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+$scope.city+'&cnt=7&APPID=0867a13b59c398d1edd05d49f440e4f0&').then(function(respons) {
-		//$scope.temp = respons.list[0].temp.day;
+	$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+$scope.city+'&cnt=7&units=metric&APPID=0867a13b59c398d1edd05d49f440e4f0').then(function(respons) {
+		$scope.cityName = respons.data.city.name +' '+respons.data.city.country;
+		$scope.lists = respons.data.list;
+		cityService.city = '';
+		// console.log(respons.data);
+	}, function(respons) {
+		$scope.cityName = respons.data.message;
 		console.log(respons);
 	});
+	$scope.date = function(date) {
+		return new Date(date * 1000);
+	}
 }]);
