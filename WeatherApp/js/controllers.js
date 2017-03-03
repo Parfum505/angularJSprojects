40d7dc987d31d51198e9f69a2ec92638e1d8c936
Pavel <parfum505@gmail.com>
2017-03-03 18:59:55 +0100

    add custom directive

--- WeatherApp/js/controllers.js
+++ WeatherApp/js/controllers.js
@@ -1,12 +1,31 @@
 var weatherControllers = angular.module('weatherControllers', ['ngResource']);
 
-weatherControllers.controller('homeController', ['$scope', 'cityService','$location', function($scope, cityService, $location) {
+weatherControllers.controller('homeController', ['$scope', 'cityService','$location','$http', function($scope, cityService, $location, $http) {
 	$scope.city = cityService.city;
 	$scope.$watch('city', function() {
 		cityService.city = $scope.city;
 	});
 	$scope.submit = function() {
 		$location.path('/forecast');
+	};
+	$scope.date = function(date) {
+		return new Date(date * 1000);
+	};
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+
+		$scope.lat = position.coords.latitude;
+		$scope.long = position.coords.longitude;
+
+		$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat='+$scope.lat+'&lon='+$scope.long+'&cnt=3&units=metric&APPID=0867a13b59c398d1edd05d49f440e4f0').then(function(respons) {
+		$scope.cityName = respons.data.city.name +' '+respons.data.city.country;
+		$scope.lists = respons.data.list;
+		console.log(respons.data);
+	}, function(respons) {
+		$scope.cityName = respons.data.message;
+		// console.log(respons);
+			});
+		});
 	}
 }]);
 
--- WeatherApp/js/script.js
+++ WeatherApp/js/script.js
@@ -17,3 +17,15 @@ myApp.service('cityService', function() {
 	this.city = '';
 });
 
+myApp.directive('weatherReport', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'directives/weatherReport.html',
+		replace: true,
+		scope: {
+			weatherDay: '=',
+			convertDate: '&',
+			dateFormat: '@'
+		}
+	}
+});
